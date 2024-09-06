608499cba5913240fae28e3f9dc09f7c8060993a
Sachintha <sudesh@orelit.com>
2024-09-07 00:31:13 +0530

    fix:dependencies version issues

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
-from api.routes.typing_analysis_routes import router as typing_analysis_router
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = FastAPI()
 
-# Register API routes
-app.include_router(typing_analysis_router)
-
 @app.get("/")
-async def root():
-    return {"message": "Welcome to the Emotion Detection API"}
+def read_root():
+    return {"message": "Server is running!"}
+
+# Get the port from environment variables
+port = int(os.getenv("PORT", 8000))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
